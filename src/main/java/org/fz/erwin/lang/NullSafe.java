21bf60dcdb45f578407ebb02279b40e38a6423bc
fengbinbin <290361638@qq.com>
2025-08-09 13:08:14 +0800

    refactor(lang): update NullSafe class and bump version to 1.2.0-RELEASE
    - Update NullSafe class to use more specific exception type in method signatures
    - Remove Vavr dependency from pom.xml
    - Update Sonatype Nexus URLs in pom.xml
    - Bump version to 1.2.0-RELEASE

--- src/main/java/org/fz/erwin/lang/NullSafe.java
+++ src/main/java/org/fz/erwin/lang/NullSafe.java
@@ -48,7 +48,7 @@ public class NullSafe {
         try { return supplier.get(); } catch (NullPointerException e) { throw exception; }
     }
 
-    public static <T> T nullThrow(Supplier<T> supplier, Supplier<RuntimeException> exception) {
+    public static <T> T nullThrow(Supplier<T> supplier, Supplier<? extends RuntimeException> exception) {
         try { return supplier.get(); } catch (NullPointerException e) { throw exception.get(); }
     }
 
@@ -69,7 +69,7 @@ public class NullSafe {
         try { runnable.run(); } catch (NullPointerException nullPoint) { throw exception; }
     }
 
-    public static void nullThrow(Runnable runnable, Supplier<RuntimeException> exception) {
+    public static void nullThrow(Runnable runnable, Supplier<? extends RuntimeException> exception) {
         try { runnable.run(); } catch (NullPointerException nullPoint) { throw exception.get(); }
     }
 
@@ -90,7 +90,7 @@ public class NullSafe {
         try { consumer.accept(arg); } catch (NullPointerException nullPoint) { throw exception; }
     }
 
-    public static <T> void nullThrow(T arg, Consumer<T> consumer, Supplier<RuntimeException> exception) {
+    public static <T> void nullThrow(T arg, Consumer<T> consumer, Supplier<? extends RuntimeException> exception) {
         try { consumer.accept(arg); } catch (NullPointerException nullPoint) { throw exception.get(); }
     }
 
@@ -119,7 +119,7 @@ public class NullSafe {
         try { return fn.apply(arg); } catch (NullPointerException nullPoint) { throw exception; }
     }
 
-    public static <T, R> R nullThrow(T arg, Function<T, R> fn, Supplier<RuntimeException> exception) {
+    public static <T, R> R nullThrow(T arg, Function<T, R> fn, Supplier<? extends RuntimeException> exception) {
         try { return fn.apply(arg); } catch (NullPointerException nullPoint) { throw exception.get(); }
     }
 
@@ -145,7 +145,7 @@ public class NullSafe {
             return () -> NullSafe.nullDefault(supplier, defaultValue);
         }
 
-        public static <T> Supplier<T> nullThrow(Supplier<T> supplier, Supplier<RuntimeException> exception) {
+        public static <T> Supplier<T> nullThrow(Supplier<T> supplier, Supplier<? extends RuntimeException> exception) {
             return () -> NullSafe.nullThrow(supplier, exception);
         }
 
@@ -161,7 +161,7 @@ public class NullSafe {
             return () -> NullSafe.nullThen(runnable, handleNull);
         }
 
-        public Runnable nullThrow(Runnable runnable, Supplier<RuntimeException> exception) {
+        public Runnable nullThrow(Runnable runnable, Supplier<? extends RuntimeException> exception) {
             return () -> NullSafe.nullThrow(runnable, exception);
         }
 
@@ -181,7 +181,7 @@ public class NullSafe {
             return t -> NullSafe.nullThrow(t, consumer, exception);
         }
 
-        public static <T> Consumer<T> nullThrow(Consumer<T> consumer, Supplier<RuntimeException> exception) {
+        public static <T> Consumer<T> nullThrow(Consumer<T> consumer, Supplier<? extends RuntimeException> exception) {
             return t -> NullSafe.nullThrow(t, consumer, exception);
         }
 
@@ -205,7 +205,7 @@ public class NullSafe {
             return t -> NullSafe.nullThrow(t, fn, exception);
         }
 
-        public static <T, R> Function<T, R> nullThrow(Function<T, R> fn, Supplier<RuntimeException> exception) {
+        public static <T, R> Function<T, R> nullThrow(Function<T, R> fn, Supplier<? extends RuntimeException> exception) {
             return t -> NullSafe.nullThrow(t, fn, exception);
         }
     }
